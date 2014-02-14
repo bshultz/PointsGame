a82c758f4ba84a05d53d88d6d4ea3ec632a6a608
Siddharth Sukumar <sidreddy88@gmail.com>
2014-02-14 11:28:50 -0600

    Changed storyboard

--- Points/CreateGroupViewController.m
+++ Points/CreateGroupViewController.m
@@ -9,9 +9,9 @@
 #import "CreateGroupViewController.h"
 #import "Parse/Parse.h"
 #import <AddressBook/AddressBook.h>
-//#import <AddressBookUI/AddressBookUI>
+#import <AddressBookUI/AddressBookUI.h>
 
-@interface CreateGroupViewController ()
+@interface CreateGroupViewController () <ABPeoplePickerNavigationControllerDelegate>
 {
     PFObject *group;
     UITextField *groupTextField;
@@ -19,11 +19,13 @@
     UIButton *addButton;
 }
 
-//@property (nonatomic, strong) ABP
+@property (nonatomic, strong) ABPeoplePickerNavigationController *addressBookController;
 
 @end
 
 @implementation CreateGroupViewController
+
+@synthesize addressBookController;
 
 
 
@@ -76,9 +78,74 @@
     }
 }
 - (IBAction)onAddFriendButtonPressed:(id)sender {
+    self.addressBookController = [[ABPeoplePickerNavigationController alloc]init];
+    [self.addressBookController setPeoplePickerDelegate:self];
+    [self presentViewController:self.addressBookController animated:YES completion:nil];
+
+}
+
+#pragma mark - ABPeoplePickerNavigationController delegate methods
+
+-(void)peoplePickerNavigationControllerDidCancel:(ABPeoplePickerNavigationController *)peoplePicker{
+    [self.addressBookController dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker shouldContinueAfterSelectingPerson:(ABRecordRef)person{
+    UIAlertView *startGamePlayMode = [[UIAlertView alloc] initWithTitle:nil message:@"This person does not have an account"  delegate:self cancelButtonTitle:nil otherButtonTitles:@"Invite", @"Cancel",  nil];
+    [startGamePlayMode show];
+    
+    //getting the persons phone numbers from the address book
+    
+    NSString *mobileNumber;
+    NSString *homeNumber;
+    
+    ABMultiValueRef phoneNumbers = ABRecordCopyValue(person, kABPersonPhoneProperty);
+     for (int j = 0; j < ABMultiValueGetCount(phoneNumbers); j++){
+         
+         if (j == 0){
+          mobileNumber = (__bridge_transfer NSString *)ABMultiValueCopyValueAtIndex(phoneNumbers,j);
+         } else if (j == 1){
+             homeNumber = (__bridge_transfer NSString *)ABMultiValueCopyValueAtIndex(phoneNumbers,j);
+         }
+         
+     }
+    
+    //getting the persons emails from the address book
+    
+    NSString *homeEmail;
+    NSString *workEmail;
+    
+    
+    ABMultiValueRef emails = ABRecordCopyValue(person, kABPersonEmailProperty);
+    for (int j = 0; j < ABMultiValueGetCount(emails); j++){
+        NSString *email = (__bridge_transfer NSString *)ABMultiValueCopyValueAtIndex(emails, j);
+        if (j == 0){
+           homeEmail = email;
+ 
+        } else if (j == 1) workEmail = email;
+    }
+   NSLog (@"persons homeEmail = %@", homeEmail);
+    
+    // need to query the Parse database to check if the current person is already has the app or not. I do this by checking the emails
+    PFQuery *query = [PFQuery queryWithClassName:@"User"];
+    [query whereKey:@"email" equalTo:homeEmail];
+    [query getFirstObjectInBackgroundWithBlock:^(PFObject *object, NSError *error) {
+        if (!object){
+            // no object present
+        } else {
+            //object is present
+        }
+    }];
     
     
     
+    
+
+    return NO;
+}
+
+-(BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker shouldContinueAfterSelectingPerson:(ABRecordRef)person property:(ABPropertyID)property identifier:(ABMultiValueIdentifier)identifier {
+    return NO;
 }
 
 
