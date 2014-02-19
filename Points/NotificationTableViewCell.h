e81ef4771ef92326e05094181de05c5277fc07ff
Siddharth Sukumar <sidreddy88@gmail.com>
2014-02-18 20:56:11 -0600

    Added custom Table View Cell for notifications page

---
+++ Points/NotificationTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  NotificationTableViewCell.h
+//  Points
+//
+//  Created by Siddharth Sukumar on 2/18/14.
+//  Copyright (c) 2014 Matthew Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationTableViewCell : UITableViewCell
+
+@end
---
+++ Points/NotificationTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  NotificationTableViewCell.m
+//  Points
+//
+//  Created by Siddharth Sukumar on 2/18/14.
+//  Copyright (c) 2014 Matthew Graham. All rights reserved.
+//
+
+#import "NotificationTableViewCell.h"
+
+@implementation NotificationTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
