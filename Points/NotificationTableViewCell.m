fc493e16e7888e08e83a08627f6275673ea990b3
Siddharth Sukumar <sidreddy88@gmail.com>
2014-02-19 19:39:02 -0600

    Added functionality for Add and Delete invite

--- Points/NotificationTableViewCell.m
+++ Points/NotificationTableViewCell.m
@@ -27,18 +27,87 @@
         self.buttonToAcceptTheInvite = [UIButton buttonWithType:UIButtonTypeRoundedRect];
         self.buttonToAcceptTheInvite.frame = CGRectMake(60.0f, 60.0f, 60, 20);
         [self.contentView addSubview:self.buttonToAcceptTheInvite];
+        [self.buttonToAcceptTheInvite addTarget:self action:@selector(addPersonToGroup) forControlEvents:UIControlEventTouchUpInside];
+
+        
 
         self.buttonToDeclineTheInvite = [UIButton buttonWithType:UIButtonTypeRoundedRect];
         self.buttonToDeclineTheInvite.frame = CGRectMake(200.0f, 60.0f, 60, 20);
          [self.contentView addSubview:self.buttonToDeclineTheInvite];
-
-        
-
-        
+        [self.buttonToDeclineTheInvite addTarget:self action:@selector(doNotAddPersonToGroup) forControlEvents:UIControlEventTouchUpInside];
 
 
     }
     return self;
+}
+
+- (void) addPersonToGroup {
+
+    PFUser *currentUser = [PFUser currentUser];
+
+    // first step is to get the group and add the currentUser to the "myMembers" relation of group object"
+    // if the first step succeeds, the second step is to add the group to the 'myGroups" relation of the user object"
+    // if the second step succeeds, the next step is to delete the invite object
+
+    PFQuery *query = [PFQuery queryWithClassName:@"Group"];
+    [query whereKey:@"objectId" equalTo:self.group.objectId];
+    [query getFirstObjectInBackgroundWithBlock:^(PFObject *object, NSError *error) {
+        if (error){
+           NSLog (@"%@ %@", error, [error userInfo]);
+        } else {
+            PFRelation *relation = [object relationForKey:@"members"];
+            [relation addObject:currentUser];
+            [object saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                if (error){
+                     NSLog (@"%@ %@", error, [error userInfo]);
+                } else {
+
+                    PFRelation *relation = [currentUser relationForKey:@"myGroups"];
+                    [relation addObject:self.group];
+                    [currentUser saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                        if (error){
+                              NSLog (@"%@ %@", error, [error userInfo]);
+                        } else {
+                            PFQuery *query = [PFQuery queryWithClassName:@"invite"];
+                            [query whereKey:@"objectId" equalTo:self.invite.objectId];
+                            [query getFirstObjectInBackgroundWithBlock:^(PFObject *object, NSError *error) {
+                                [object deleteInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                                    if (error){
+                                        NSLog (@"%@ %@", error, [error userInfo]);
+                                    }
+                                }];
+                            }];
+
+                        }
+                    }];
+
+                }
+            }];
+        }
+    }];
+
+
+
+}
+
+- (void) doNotAddPersonToGroups {
+
+    PFQuery *query = [PFQuery queryWithClassName:@"invite"];
+    [query whereKey:@"objectId" equalTo:self.invite.objectId];
+    [query getFirstObjectInBackgroundWithBlock:^(PFObject *object, NSError *error) {
+        [object deleteInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+            if (error){
+                NSLog (@"%@ %@", error, [error userInfo]);
+                
+            }
+        }];
+    }];
+
+
+
+
+
+
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
